7b8198c70ab371d6f84b9e65f5a05bf53f5240be
fhs <fattah.hanafisheikhha@ontariotechu.net>
2023-01-13 14:00:41 -0500

    Solved second challenge of day04

--- adventOfCode/2015/day04/day04.cpp
+++ adventOfCode/2015/day04/day04.cpp
@@ -23,11 +23,33 @@ int32_t firstChallenge(std::string s)
     return res;
 };
 
+int32_t secondChallenge(std::string s)
+{
+    int32_t res = 0;
+    bool found = false;
+    std::string tmp;
+    unsigned char hash[MD5_DIGEST_LENGTH];
+    do
+    {
+        ++res;
+        tmp = s + std::to_string(res);
+        MD5((unsigned char *)tmp.c_str(), tmp.size(), hash);
+
+        found = hash[0] == 0 && hash[1] == 0 && hash[2] == 0;
+
+    } while (!found);
+
+    return res;
+};
+
 int main()
 {
     std::string s = "bgvyzdsv";
     int32_t s1 = firstChallenge(s);
     std::cout << s1 << std::endl;
+
+    int32_t s2 = secondChallenge(s);
+    std::cout << s2 << std::endl;
 
     return 0;
 }
