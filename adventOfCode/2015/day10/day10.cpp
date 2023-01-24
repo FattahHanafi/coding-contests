53ae49d6545028bf1e240e93c67ed85070e26b8b
fhs <fattah.hanafisheikhha@ontariotechu.net>
2023-01-24 16:08:11 -0500

    Solved second challenge of day10

--- adventOfCode/2015/day10/day10.cpp
+++ adventOfCode/2015/day10/day10.cpp
@@ -22,10 +22,34 @@ size_t firstChallenge(std::string s)
     return res.length();
 };
 
+size_t secondChallenge(std::string s)
+{
+    std::string res;
+
+    for (size_t i = 0; i < 50; ++i)
+    {
+        res = "";
+        while (s.length())
+        {
+            size_t idx = s.find_first_not_of(s[0]);
+            idx = std::min(idx, s.length());
+            res += '0' + idx;
+            res += s[0];
+            s.erase(0, idx);
+        }
+        s = res;
+    }
+
+    return res.length();
+};
+
 int main()
 {
     std::string inp = "3113322113";
 
     size_t s1 = firstChallenge(inp);
     std::cout << s1 << std::endl;
+
+    size_t s2 = secondChallenge(inp);
+    std::cout << s2 << std::endl;
 }
